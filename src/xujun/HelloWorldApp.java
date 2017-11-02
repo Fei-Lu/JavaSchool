32cbd04a5bd1d6161b567fd63806fca2d6426d3b
Jun Xu <Jun Xu@LuLab-PC-02>
2017-11-02 19:43:38 +0800

    My first class

---
+++ src/xujun/HelloWorldApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xujun;
+
+/**
+ * @author Jun Xu
+ */
+public class HelloWorldApp {
+    
+    
+    public static void main (String[] args) {
+        System.out.println("Hello world!");
+        int a = 3;
+        int b = 2;
+    }
+}

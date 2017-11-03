79499d8177d33a1b27be3aa95db210cebf456883
Jun Xu <Jun Xu@LuLab-PC-02>
2017-11-03 16:41:17 +0800

    variable

---
+++ src/xujun/ExerciseDay3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xujun;
+
+/**
+ *
+ * @author Jun Xu
+ */
+public class ExerciseDay3 {
+     public static void main (String[] args) {
+         int a=2;
+         System.out.println("a="+a);         
+     }
+      
+            
+}

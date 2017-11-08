232208bc7c2368e75ffd033266e32b9b026befde
cauxu <cauxu@DESKTOP-ASV8EHF>
2017-11-08 20:24:39 +0800

    JavaSchool

---
+++ src/chenxiao/ControlDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chenxiao;
+
+/**
+ *
+ * @author cauxu
+ */
+public class ControlDemo {
+    public static void main(String[] args){
+        int aNumber=3;
+        if (aNumber >= 0)
+        if (aNumber == 0)
+        System.out.println("first string");
+       else 
+        System.out.println("second string");
+        System.out.println("third string");
+    }
+}

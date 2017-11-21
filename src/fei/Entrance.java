b9cad8f1e03f1f60f5ada560916a3c2046624c1f
Fei Lu <dr.lufei@gmail.com>
2017-11-21 19:00:58 +0800

    some demo of nested classes

---
+++ src/fei/Entrance.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fei;
+
+/**
+ *
+ * @author feilu
+ */
+public class Entrance {
+    
+    public static void main (String[] args) {
+        //System.out.println("Hello, world!");
+        //NestedClassExample nce = new NestedClassExample ();
+        //LocalClassExample.validatePhoneNumber("123-456-7890", "456-7890");
+        AnonymousClassExample ace = new AnonymousClassExample();
+        ace.sayHello();
+    }
+
+}

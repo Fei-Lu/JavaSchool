2c79817cda38721b3d68644107322d0882c875d0
Aoyue <Aoyue@LULAB-PC-04>
2017-12-14 18:06:36 +0800

    IOStreamAoyue

---
+++ src/aoyue/IOStreamAoyue.java
@@ -0,0 +1,246 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aoyue;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aoyue
+ */
+public class IOStreamAoyue {
+    public IOStreamAoyue () throws IOException {
+        
+//        this.CopyBytes();
+//        this.CopyCharacters();
+//        this.CopyLines ();
+//        this.ScanXan ();
+        //this. ScanSum();
+//        this.Root ();
+//        this.Root2 ();
+//        this.Password ();
+       this.DataOutputStream ();
+       
+    }
+       
+    
+    
+    
+    public static void main(String[] args) throws IOException {
+        new IOStreamAoyue ();
+    
+    }
+
+    private void CopyBytes() throws IOException {
+            FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("xanadu.txt");
+            out = new FileOutputStream("outagain.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+    
+    private void CopyCharacters() throws IOException {
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+
+    private void CopyLines() throws IOException {
+        /*throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.*/
+         BufferedReader inputStream = null;
+        PrintWriter outputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                outputStream.println(l);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    
+    
+    }
+
+    private void ScanXan() throws FileNotFoundException {
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("xanadu.txt"))); //将对象传递给构造器BufferedReader
+
+            while (s.hasNext()) {  //Returns true if this scanner has another token in its input.
+                System.out.println(s.next());
+               //System.out.println(s.useDelimiter(",\\s*"));
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void ScanSum() throws FileNotFoundException {
+        Scanner s = null;
+        double sum = 0;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("usnumbers.txt")));
+            s.useLocale(Locale.US);
+
+            while (s.hasNext()) {
+                if (s.hasNextDouble()) {
+                    sum += s.nextDouble();
+                } else {
+                    s.next();
+                }   
+            }
+        } finally {
+            s.close();
+        }
+
+        System.out.println(sum);
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Root() {
+        int i = 2;
+        double r = Math.sqrt(i);
+        
+        System.out.print("The square root of ");
+        System.out.print(i);
+        System.out.print(" is ");
+        System.out.print(r);
+        System.out.println(".");
+
+        i = 5;
+        r = Math.sqrt(i);
+        System.out.println("The square root of " + i + " is " + r + ".");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Root2() {
+         int i = 2;
+        double r = Math.sqrt(i);
+        //d格式化一个整数值作为一个十进制值。 f格式化浮点值作为一个十进制值。 n输出一个特定于平台的行终止符。
+        System.out.format("The square root of %d is %f.%n", i, r); // ?????i 和r 的作用??
+        System.out.format("%f, %1$+020.10f %n", Math.PI); // < 格式化前一个转换符所描述的参数  ???????
+        System.out.format("%f, %<+020.10f %n", Math.PI); // $ 被格式化的参数索引  ??????????
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Password() {
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+
+        String login = c.readLine("Enter your login: ");
+        char [] oldPassword = c.readPassword("Enter your old password: ");
+
+        if (verify(login, oldPassword)) {
+            boolean noMatch;
+            do {
+                char [] newPassword1 = c.readPassword("Enter your new password: ");
+                char [] newPassword2 = c.readPassword("Enter new password again: ");
+                noMatch = ! Arrays.equals(newPassword1, newPassword2);
+                if (noMatch) {
+                    c.format("Passwords don't match. Try again.%n");
+                } else {
+                    change(login, newPassword1);
+                    c.format("Password for %s changed.%n", login);
+                }
+                Arrays.fill(newPassword1, ' ');
+                Arrays.fill(newPassword2, ' ');
+            } while (noMatch);
+        }
+
+        Arrays.fill(oldPassword, ' ');
+    }
+    
+    // Dummy change method.
+    static boolean verify(String login, char[] password) {
+        // This method always returns
+        // true in this example.
+        // Modify this method to verify
+        // password according to your rules.
+        return true;
+    }
+
+    // Dummy change method.
+    static void change(String login, char[] password) {
+        // Modify this method to change
+        // password according to your rules.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void DataOutputStream() throws IOException {
+        File file = new File("C:\\Users\\Aoyue\\Documents\\NetBeansProjects\\JavaSchool\\netbeans" + File.separator + "hello.txt");
+        char[] ch = { 'A', 'B', 'C' };
+        DataOutputStream out = null;
+        out = new DataOutputStream(new FileOutputStream(file));
+        for(char temp : ch){
+            out.writeChar(temp);
+        }
+        out.close();
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    }  
+
+    

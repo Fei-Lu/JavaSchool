9c4c710cc2781550360dd3c8fe1e8b2c16bd5fd9
Fei Lu <dr.lufei@gmail.com>
2018-09-23 00:22:36 +0800

    Delete OperateCar

--- src/fei/DatabaseExample.java
+++ src/fei/DatabaseExample.java
@@ -5,12 +5,12 @@
  */
 package fei;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 /**
  *
